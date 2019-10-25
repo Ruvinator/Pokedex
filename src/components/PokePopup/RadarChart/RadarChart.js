4a5db5048e9a541303c39da6d448bfc0ad59799c
Igor <ruvinovigor@gmail.com>
2019-10-25 01:32:23 -0400

    Radar chart in popup (assets)
    - Initialize radar chart logic and members
    - Add assets for nodes (for each stat)
    - Temporarily position nodes in radar chart view

--- src/components/PokePopup/RadarChart/RadarChart.js
+++ src/components/PokePopup/RadarChart/RadarChart.js
@@ -3,10 +3,23 @@ import styles from './RadarChart.module.css';
 
 class RadarChart extends PureComponent {
     render() {
-        const radarDir = require('../../../assets/images/RadarChart/RadarChartFrame.svg')
+        const assetsDir    = '';
+        const radarDir     = require('../../../assets/images/RadarChart/RadarChartFrame.svg');
+        const nodeHPDir    = require('../../../assets/images/RadarChart/NodeHP.png');
+        const nodeAtkDir   = require('../../../assets/images/RadarChart/NodeAtk.png');
+        const nodeDefDir   = require('../../../assets/images/RadarChart/NodeDef.png');
+        const nodeSpADir   = require('../../../assets/images/RadarChart/NodeSpA.png');
+        const nodeSpDDir   = require('../../../assets/images/RadarChart/NodeSpD.png');
+        const nodeSpeedDir = require('../../../assets/images/RadarChart/NodeSpeed.png');
         return (
-            <div className={styles.RadarOutline}>
-                <img src={radarDir} alt='' />
+            <div className={styles.RadarChart}>
+                <img src={radarDir}     alt='' className={styles.RadarOutline} />
+                <img src={nodeHPDir}    alt='' className={[styles.Node,    styles.HP].join(' ')} />
+                <img src={nodeAtkDir}   alt='' className={[styles.Node,   styles.Atk].join(' ')} />
+                <img src={nodeDefDir}   alt='' className={[styles.Node,   styles.Def].join(' ')} />
+                <img src={nodeSpADir}   alt='' className={[styles.Node,   styles.SpA].join(' ')} />
+                <img src={nodeSpDDir}   alt='' className={[styles.Node,   styles.SpD].join(' ')} />
+                <img src={nodeSpeedDir} alt='' className={[styles.Node, styles.Speed].join(' ')} />
             </div>
         )
     }
